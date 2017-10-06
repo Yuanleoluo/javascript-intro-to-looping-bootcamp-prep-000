115bcf335d45b4221b913566b5ba2a63743de43c
Yuanleoluo <leoyuanluo@gmail.com>
2017-10-06 17:47:29 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,12 +2,7 @@
 
 function forLoop(a){
   for(let i = 0; i<25; i++){
-    if (i=1){
-      a[i]= 'I am 1 strange loops'
-    }
-    else {
       a[i] = 'I am ${i} strange loops'
-    }
   }
   return a
 }
